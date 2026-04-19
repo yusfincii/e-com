e8c498f0a9b73e3a268c84811f2d06e09e486b2a
yusuff <ysfnc27@gmail.com>
2026-04-19 22:46:40 +0300

    fix-templates

---
+++ order-service/src/main/java/com/ecom/order/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.ecom.order;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order Service API")
+                        .description("E-Commerce Order Service")
+                        .version("v1.0"));
+    }
+}
